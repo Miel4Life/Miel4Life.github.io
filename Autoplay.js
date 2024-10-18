a088e95c5545f0dbc9f7cd512783878ddbf0365e
Miel4Life <BlankersMeriel@gmail.com>
2024-10-18 13:14:08 +0200

    Create Autoplay.js

---
+++ Autoplay.js
@@ -0,0 +1,12 @@
+onload = () => {
+var sound = new Howl({
+  src: ['EI-EI-EI.png'],
+  autoplay: true,
+  loop: true,
+  volume: 1,
+  onend: function() {
+//    console.log('Finished!');
+  }
+});
+
+}
